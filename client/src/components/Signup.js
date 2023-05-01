49a35933c47ca4725a32f14ad4f63c18ebb28af9
SaifAshraf22 <saifashraf808@gmail.com>
2023-05-01 13:32:01 +0200

    create fields in sign up page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,35 @@
 import React from 'react'
-
+import {Formik,Form} from 'formik'
+import TextField from './TextField'
 function signup() {
   return (
-    <div>
-        <h1>Sign Up</h1>
-    </div>
+    <Formik 
+      initialValues={{
+        firstName:'',
+        lastName:'',
+        email:'',
+        password:'',
+        confirmPassword:''
+    }
+        
+
+      }>
+        {formik =>(
+            <div>
+                <h1 className='my-4 font-weight-bold-display-4'>Sign Up</h1>
+                <Form>
+                <TextField label='First Name' name='firstName' type='text'/>
+                <TextField label='Last Name' name='lastName' type='text'/>
+                <TextField label='Email' name='Email' type='email'/>
+                <TextField label='Password' name='Password' type='password'/>
+                <TextField label='Confirm Password' name='confirmPassword' type='password'/>
+                <button className='btn btn-dark mt-3'type='submit'>Register</button> 
+                <button className='btn btn-danger mt-3 ml-3'type='reset'>Reset</button> 
+
+                </Form>
+            </div>
+        )}
+    </Formik>
   )
 }
 
