4b9a078cf03c3efc2a8a65aa837481448bbdbe8b
SaifAshraf22 <saifashraf808@gmail.com>
2023-05-01 12:19:32 +0200

    modify the main project

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import img from './assets/rocket.png'
+import Signup from './components/Signup';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className='col1'>
+          <Signup/>
+        </div>
+        <div className='col2'>
+          <img className='img ' src={img}/>
+        </div>
+      </div>
     </div>
   );
 }
