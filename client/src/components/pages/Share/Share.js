f7242def67d6ff3d1e505d0a0f698395ded6dbdb
SaifAshraf22 <saifashraf808@gmail.com>
2023-05-04 04:06:29 +0200

    add share page

---
+++ client/src/components/pages/Share/Share.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import "./share.css"
+import {PermMedia, PremModia,Label,Room,EmojiEmotions } from '@mui/icons-material'
+function Share() {
+  return (
+    <div className='share'>
+        <div className='shareWrapper'>
+            <div className='shareTop'>
+                <img className='shareProfileImg'src="https://th.bing.com/th/id/R.da1ce16d58c86a77cfc5bdeb8d5faedc?rik=JEHfqc%2fff5c67A&pid=ImgRaw&r=0" alt=''/>
+                <input 
+                placeholder="What's in your mind ? "
+                className='shareInput'
+                />  
+            </div>
+            <hr className='shareHr'/>
+            <div className='shareBottom'>
+                <div className='shareOptions'>
+                    <div className='shareOption'>
+                        <PermMedia htmlColor='tomato' className='shareIcon'/>
+                        <span className="shareOptionText">Photo or Video</span>
+                    </div>
+                    <div className='shareOption'>
+                        <Label htmlColor='blue' className='shareIcon'/>
+                        <span className="shareOptionText">Tag</span>
+                    </div>
+                    <div className='shareOption'>
+                        <Room htmlColor='green' className='shareIcon'/>
+                        <span className="shareOptionText">Location</span>
+                    </div>
+                    <div className='shareOption'>
+                        <EmojiEmotions htmlColor='goldenrod' className='shareIcon'/>
+                        <span className="shareOptionText">Feelings</span>
+                    </div>
+                </div>
+                <button className='shareButton'>Share</button>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Share
