ab543e19ef8a8b8a36667c04e29455713c5b1834
SaifAshraf22 <saifashraf808@gmail.com>
2023-05-04 14:20:55 +0200

    change files to JSX

--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Topbar from '../../Navbar/Topbar'
 import './home.css'
 import Feeds from '../Feeds/Feeds'
+import Sidebar from '../Sidebar/Sidebar'
+import Rightbar from '../Rightbar/Rightbar'
 
 function Home() {
   return (
     <>
     <Topbar/>
       <div className='homeContainer'>
+        <Sidebar/>
         <Feeds/>
+        <Rightbar/>
       </div>
       </>
   )
