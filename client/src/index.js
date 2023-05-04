bfee84a4450ed9ff1915eaf6e333b1b5161eede2
yogilany <yousef.gilany021@eng-st.cu.edu.eg>
2023-05-04 18:17:29 +0300

    added routes

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import'bootstrap/dist/css/bootstrap.min.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/pages/Home/Home';
+import RegisterPage from './components/Register';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/signup",
+    element: <RegisterPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode >
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
